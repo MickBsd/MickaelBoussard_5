d415c95dae2471bc065d8c4428c9a7ef3fa81185
Mickael Bsd <mickael.boussard@orange.fr>
2021-11-20 12:43:23 +0100

    fix index.js + creation of recuperation fonction from products

--- front/js/index.js
+++ front/js/index.js
@@ -1,21 +1,20 @@
-import {testProducts, getProducts} from './function.js';
+
+//Récupération du tableau de produits disponibles
+getProducts();
 
 
-// //Récupération des données de l'api 
-// async function getProducts() {
-//     let products = await fetch('http://localhost:3000/api/products');
-//     return products.json();
-// };
-
-testProducts();
-
+//Création des articles via la liste récupérée précédemment
 creationProducts();
 
+async function getProducts() {
+    let products = await fetch('http://localhost:3000/api/products');
+    console.log("Les produits ont été récupérés !")
+    return products.json();
+}
 
 async function creationProducts() {
     let result = await getProducts()
     .then( (product) => {
-        console.log("test");
         for (let i=0; i < product.length; i++) {		
 
             // Insertion de l'élément "a"
@@ -46,42 +45,5 @@ async function creationProducts() {
             productDescription.innerHTML = product[i].description;
         }
     });
+    console.log("Les produits ont été crées !");
 }
-
-
-
-// fetch('http://localhost:3000/api/products')
-// 	.then((response) => response.json())
-// 	.then((product) => {
-		
-//         //Création des articles via une boucle for
-// 		for (let i=0; i < product.length; i++) {		
-
-// 			// Insertion de l'élément "a"
-//             let productLink = document.createElement("a");
-//             document.querySelector(".items").appendChild(productLink);
-//             productLink.href = `product.html?id=${product[i]._id}`;
-
-//             // Insertion de l'élément "article"
-//             let productArticle = document.createElement("article");
-//             productLink.appendChild(productArticle);
-
-//             // Insertion de l'image
-//             let productImg = document.createElement("img");
-//             productArticle.appendChild(productImg);
-//             productImg.src = product[i].imageUrl;
-//             productImg.alt = product[i].altTxt;
-
-//             // Insertion du titre "h3"
-//             let productName = document.createElement("h3");
-//             productArticle.appendChild(productName);
-//             productName.classList.add("productName");
-//             productName.innerHTML = product[i].name;
-
-//             // Insertion de la description "p"
-//             let productDescription = document.createElement("p");
-//             productArticle.appendChild(productDescription);
-//             productDescription.classList.add("productName");
-//             productDescription.innerHTML = product[i].description;
-// 		}
-//   	});
--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,17 @@
-const productId = new URL(window.location.href).searchParams;; 
-console.log("j'ai recuperer l'id suivant : " + productId);
 
-//productId va permettre de chercher les données via l'api
+//Récupération de l'id via les paramètres de l'url
+const idProduct = new URL(window.location.href).searchParams.get("id");
+console.log("j'ai recuperer l'id suivant : " + idProduct);
+
+getArticle();
+
+//Récupération de l'article grace a l'id
+function getArticle() {
+    fetch("http://localhost:3000/api/products/" + idProduct)
+    .then((response) => response.json())    
+    .then(product => {
+        console.log("Ce qui me donne le kanap suivant : " + product.name);
+        });
+
+
+}
