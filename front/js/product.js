39ec4444ea6ee3f76f256f6780c8b1ecbeff9102
Mickael Bsd <mickael.boussard@orange.fr>
2021-11-21 14:39:56 +0100

    display product page ok, in progress : selelction of an artice and put it on local storage

--- front/js/product.js
+++ front/js/product.js
@@ -1,17 +1,35 @@
-
 //Récupération de l'id via les paramètres de l'url
 const idProduct = new URL(window.location.href).searchParams.get("id");
-console.log("j'ai recuperer l'id suivant : " + idProduct);
+
+//Récupération des sélecteurs pour les futurs modifications
+let imgProduct = document.querySelector(".item__img");
+let img = document.createElement("img");
+imgProduct.appendChild(img);
+let titleProduct = document.getElementById("title");
+let priceProduct = document.getElementById("price");
+let descriptionProduct = document.getElementById("description");
+let colorsProduct = document.getElementById("colors");
 
 getArticle();
 
-//Récupération de l'article grace a l'id
-function getArticle() {
-    fetch("http://localhost:3000/api/products/" + idProduct)
+//Récupération de l'article grace a l'id + affichage des données de ce dernier
+async function getArticle() {
+     await fetch("http://localhost:3000/api/products/" + idProduct)
     .then((response) => response.json())    
     .then(product => {
-        console.log("Ce qui me donne le kanap suivant : " + product.name);
-        });
+        console.log(product);
 
+        img.setAttribute("src", product.imageUrl);
+        img.setAttribute("alt", product.altTxt);    
+        titleProduct.innerHTML = product.name;
+        priceProduct.innerHTML = product.price;
+        descriptionProduct.innerHTML = product.description;
 
+        for (let i=0; i < product.colors.length; i++) {
+            let color = document.createElement("option");
+            color.setAttribute("value", product.colors[i]);
+            color.innerHTML = product.colors[i];
+            colorsProduct.appendChild(color);
+        }  
+    });          
 }
